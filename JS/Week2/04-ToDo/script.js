c3de050a9e1fc87c518a7189e58d68a0a67cde93
silentshutter5 <168572865+silentshutter5@users.noreply.github.com>
2024-06-06 13:31:27 +0300

    added alert

--- JS/Week2/04-ToDo/script.js
+++ JS/Week2/04-ToDo/script.js
@@ -1,16 +1,24 @@
 const taskInput = document.getElementById("task-input");
 const addTaskBtn = document.getElementById("add-task-btn");
 const taskList = document.getElementById("task-list");
+const removeTaskBtn = document.getElementById("remove-task-btn");
 addTaskBtn.addEventListener("click", function() {
   const taskText = taskInput.value;
   if (taskText.trim() !== "") {
     const taskItem = document.createElement("li");
     taskItem.innerText = taskText;
+    taskItem.addEventListener("click", function(){
+      if(confirm('you sure?')){
+        taskItem.remove();
+      }
+    })
+
    
     taskList.appendChild(taskItem);
     taskInput.value = "";
   }
 });
+
 
 taskList.addEventListener("click", function(event) {
   if (event.target.tagName.toLowerCase() === "li") {
